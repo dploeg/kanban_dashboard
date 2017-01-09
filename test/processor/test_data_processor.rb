d8e65dbb1495eac629829cc154cf1b50bf823e4c
Daniel Ploeg <dploeg@gmail.com>
2017-01-10 10:28:34 +1100

    refactor for consistency

--- test/processor/test_data_processor.rb
+++ test/processor/test_data_processor.rb
@@ -7,31 +7,6 @@ require_relative '../../lib/processor/data_processor'
 
 class TestDataProcessor < Minitest::Test
 
-  def setup_readers
-    @reader = MiniTest::Mock.new
-    @reader.expect :read_data, nil
-    @reader.expect :work_items, @work_items
-  end
-
-  def setup_widget_processors
-    @first_widget_processor = MiniTest::Mock.new
-    @second_widget_processor = MiniTest::Mock.new
-    process_work_items(@first_widget_processor)
-    process_work_items(@second_widget_processor)
-
-    @widget_processors = [@first_widget_processor, @second_widget_processor]
-  end
-
-  def process_work_items(widget_processor)
-    widget_processor.expect :process, nil, [@work_items]
-    widget_processor.expect :output, nil
-  end
-
-  def setup_work_items
-    @work_items = [WorkItem.new(:start_date => "10/3/16", :complete_date => "21/3/16"),
-                   WorkItem.new(:start_date => "15/3/16", :complete_date => "21/3/16")]
-  end
-
   context 'DataProcessor' do
 
     setup do
@@ -49,6 +24,31 @@ class TestDataProcessor < Minitest::Test
       @second_widget_processor.verify
     end
 
+  end
+
+  private def setup_readers
+    @reader = MiniTest::Mock.new
+    @reader.expect :read_data, nil
+    @reader.expect :work_items, @work_items
+  end
+
+  private def setup_widget_processors
+    @first_widget_processor = MiniTest::Mock.new
+    @second_widget_processor = MiniTest::Mock.new
+    process_work_items(@first_widget_processor)
+    process_work_items(@second_widget_processor)
+
+    @widget_processors = [@first_widget_processor, @second_widget_processor]
+  end
+
+  private def process_work_items(widget_processor)
+    widget_processor.expect :process, nil, [@work_items]
+    widget_processor.expect :output, nil
+  end
+
+  private def setup_work_items
+    @work_items = [WorkItem.new(:start_date => "10/3/16", :complete_date => "21/3/16"),
+                   WorkItem.new(:start_date => "15/3/16", :complete_date => "21/3/16")]
   end
 
 end
