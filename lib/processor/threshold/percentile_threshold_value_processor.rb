65e0233570f9a88e8401abc9ff7364973cc714d3
Daniel Ploeg <dploeg@gmail.com>
2017-01-11 12:30:18 +1100

    Added a few more tests

--- lib/processor/threshold/percentile_threshold_value_processor.rb
+++ lib/processor/threshold/percentile_threshold_value_processor.rb
@@ -3,8 +3,10 @@ require_relative '../processor_utils'
 class PercentileThresholdValueProcessor
   include ProcessorUtils
 
-  def initialize
-    @percentile = 95
+  attr_reader :percentile
+
+  def initialize(percentile = 95)
+    @percentile = percentile
   end
 
   def process(work_items, *thresholds)
@@ -12,26 +14,35 @@ class PercentileThresholdValueProcessor
     @percentile_values = populate_percentile_lead_times_from_work_items(sort_into_classes_of_service(work_items), @percentile)
 
     thresholds.each { |threshold|
-      class_of_service = threshold.class_of_service
-      if class_of_service.nil?
-        class_of_service = ProcessorUtils::STANDARD_CLASS_OF_SERVICE
-      end
+      class_of_service = retrieve_class_of_service(threshold)
       actual = @percentile_values[class_of_service]
-      if(threshold.type == Threshold::LOWER)
-        if actual < threshold.value #question as to whether to push this down into the threshold itself
-          warnings.push(build_warning(actual, threshold))
-        end
-      else
-        if actual > threshold.value
-          warnings.push(build_warning(actual, threshold))
-        end
-      end
+      check_threshold(actual, threshold, warnings, class_of_service)
     }
     warnings
   end
 
-  private def build_warning(actual, threshold)
-    ThresholdWarning.new("Lead Time " + @percentile.to_s + " percentile",
+  private def check_threshold(actual, threshold, warnings, class_of_service)
+    if (threshold.type == Threshold::LOWER)
+      if actual < threshold.value #question as to whether to push this down into the threshold itself
+        warnings.push(build_warning(actual, threshold, class_of_service))
+      end
+    else
+      if actual > threshold.value
+        warnings.push(build_warning(actual, threshold, class_of_service))
+      end
+    end
+  end
+
+  private def retrieve_class_of_service(threshold)
+    class_of_service = threshold.class_of_service
+    if class_of_service.nil?
+      class_of_service = ProcessorUtils::STANDARD_CLASS_OF_SERVICE
+    end
+    class_of_service
+  end
+
+  private def build_warning(actual, threshold, class_of_service)
+    ThresholdWarning.new("Lead Time " + @percentile.to_s + " percentile - " + class_of_service ,
                          "has exceeded " + threshold.type.downcase + " threshold of " + threshold.value.to_s + " with value of " + actual.to_s)
   end
 end
--- test/processor/thresholds/test_percentile_threshold_value_processor.rb
+++ test/processor/thresholds/test_percentile_threshold_value_processor.rb
@@ -30,7 +30,7 @@ class TestPercentileThresholdValueProcessor < Minitest::Test
       warnings = processor.process(@work_items, @threshold3)
 
       assert_equal 1, warnings.size
-      assert_equal "Lead Time 95 percentile", warnings[0].label
+      assert_equal "Lead Time 95 percentile - Standard", warnings[0].label
       assert_equal "has exceeded upper control limit threshold of 9 with value of 10", warnings[0].value
     end
 
@@ -39,12 +39,43 @@ class TestPercentileThresholdValueProcessor < Minitest::Test
       warnings = processor.process(@work_items, @threshold4)
 
       assert_equal 1, warnings.size
-      assert_equal "Lead Time 95 percentile", warnings[0].label
+      assert_equal "Lead Time 95 percentile - Standard", warnings[0].label
       assert_equal "has exceeded lower control limit threshold of 11 with value of 10", warnings[0].value
     end
 
-    should "process multiple items for class of service" do
+    should "process multiple items for different classes of service" do
+      @work_items = [WorkItem.new(:start_date => "10/3/16", :complete_date => "21/3/16"),
+                           WorkItem.new(:start_date => "15/3/16", :complete_date => "21/3/16"),
+                           WorkItem.new(:start_date => "12/3/16", :complete_date => "14/4/16"),
+                           WorkItem.new(:start_date => "19/3/16", :complete_date => "19/4/16"),
+                           WorkItem.new(:start_date => "21/3/16", :complete_date => "17/4/16"),
+                           WorkItem.new(:start_date => "28/3/16", :complete_date => "22/4/16"),
+                           WorkItem.new(:start_date => "2/4/16", :complete_date => "25/4/16", :class_of_service => TestConstants::STANDARD),
+                           WorkItem.new(:start_date => "3/4/16", :complete_date => "12/4/16"),
 
+                           WorkItem.new(:start_date => "3/4/16", :complete_date => "12/4/16", :class_of_service => TestConstants::EXPEDITE),
+                           WorkItem.new(:start_date => "2/4/16", :complete_date => "13/4/16", :class_of_service => TestConstants::FIXED_DATE),
+                           WorkItem.new(:start_date => "5/4/16", :complete_date => "25/4/16", :class_of_service => TestConstants::INTANGIBLE),
+                           WorkItem.new(:start_date => "6/4/16", :complete_date => "13/4/16", :class_of_service => TestConstants::EXPEDITE),
+                           WorkItem.new(:start_date => "7/4/16", :complete_date => "14/4/16", :class_of_service => TestConstants::EXPEDITE),
+                           WorkItem.new(:start_date => "6/4/16", :complete_date => "28/4/16", :class_of_service => TestConstants::FIXED_DATE),
+                           WorkItem.new(:start_date => "13/4/16", :complete_date => "12/5/16", :class_of_service => TestConstants::INTANGIBLE)]
+      @threshold5 = Threshold.new(:type => Threshold::UPPER, :value => 7, :processor => "first", :class_of_service => TestConstants::EXPEDITE)
+
+      processor = PercentileThresholdValueProcessor.new
+      warnings = processor.process(@work_items, *[@threshold3, @threshold5])
+
+      assert_equal 2, warnings.size
+      assert_equal "Lead Time 95 percentile - Standard", warnings[0].label
+      assert_equal "has exceeded upper control limit threshold of 9 with value of 32", warnings[0].value
+      assert_equal "Lead Time 95 percentile - Expedite", warnings[1].label
+      assert_equal "has exceeded upper control limit threshold of 7 with value of 8", warnings[1].value
+    end
+
+    should "accept optional percentile value for constructor" do
+      processor = PercentileThresholdValueProcessor.new(85)
+
+      assert_equal 85, processor.percentile
     end
 
   end
