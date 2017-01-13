d4bba5b69bb10b80f2297906f417a8f2e99159c4
Daniel Ploeg <dploeg@gmail.com>
2017-01-13 11:40:11 +1100

    Added weibull curve to generation algorithm

--- utils/work_item_generator.rb
+++ utils/work_item_generator.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'simple-random'
 require_relative '../lib/model/work_item'
 
 
@@ -18,6 +19,12 @@ def determine_class_of_service()
   class_of_service
 end
 
+def get_complete_date_additional_days(class_of_service, classes_of_service_max_days)
+  r = SimpleRandom.new
+  r.set_seed
+  classes_of_service_max_days[class_of_service] * r.weibull(2,4) /10
+end
+
 num_items = 100
 
 first_date = DateTime.strptime("1/1/16", WorkItem::DATE_FORMAT)
@@ -34,12 +41,13 @@ classes_of_service_max_days = {EXPEDITE => 10, STANDARD => 42, FIXED_DATE => 30,
 
 work_items = Array.new
 item_count = 0
+
 (0..num_items).each {
   class_of_service = determine_class_of_service
 
 
   start_date = rand(first_date..last_date)
-  complete_date = rand(start_date..start_date+classes_of_service_max_days[class_of_service])
+  complete_date = rand(start_date..start_date+get_complete_date_additional_days(class_of_service, classes_of_service_max_days))
 
   work_items.push(WorkItem.new(:start_date => start_date.strftime(WorkItem::DATE_FORMAT), :complete_date => complete_date.strftime(WorkItem::DATE_FORMAT), :class_of_service => class_of_service))
   item_count+=1
